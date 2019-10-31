e21299cfb314239badbc665ef644a3323879fd89
luqmanMohammed <mohammed.luqman@pearson.com>
2019-10-31 16:40:43 +0530

    Add introspect token function

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -2,6 +2,18 @@ const jwtHelper = require("../utils/JWTHelper")
 const {JWT_SECRET} = process.env
 class TokenController {
     introspectToken(req,res,next){
-        
+        const {token} = req.body;
+        try {
+            const {role,email} = await jwtHelper.verifyToken(token,JWT_SECRET);
+            // check revoke
+            return res.status(200).json({
+                role,
+                "active" : true
+            });
+        }catch(e) {
+            return res.status(200).json({
+                "active": false
+            });
+        }    
     }
 }
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,23 +1,20 @@
-const jwt = require("jwt");
-
+const jwtHelper = require("../utils/JWTHelper");
 const { JWT_SECRET } = process.env;
 
-const authenticateToken = (req, res, next, role) => {
+const authenticateToken = async (req, res, next, role) => {
   let auth = req.headers.authorization;
   if (auth) {
     const token = auth.split(" ")[1];
     let bearer = null;
     try {
-      bearer = jwt.verify(token, JWT_SECRET);
-      //check if revoked
-
+      bearer = await jwtHelper.verifyToken(token,JWT_SECRET);
     } catch (error) {
-      res.status(401).json({ msg: "Invalid Token" });
+      res.status(401).send("Invalid Token");
       return;
     }
     if (bearer && bearer.role === role) next();
-    else res.status(403).json({ msg: "Unathorized" });
-  } else res.status(401).json({ msg: "Invalid Token" });
+    else res.status(403).send("Unathorized");
+  } else res.status(401).send("Invalid Token");
 };
 
 module.exports.rs_auth = (req,res,next) => authenticateToken(req,res,next,"resource_server");
