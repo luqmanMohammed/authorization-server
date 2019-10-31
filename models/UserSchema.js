5122980b53a47d0d63953603f6d1b78240a845c7
luqmanMohammed <mohammed.luqman@pearson.com>
2019-10-31 11:03:39 +0530

    Add UserSchema

---
+++ models/UserSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const roles = ["resource_server","admin","moderator","student"];
+const UserSchema = new mongoose.Schema({
+    email: {
+        required: true,
+        type: String,
+        unique: true
+    },
+    password: {
+        required: true,
+        type: String
+    },
+    role: {
+        required: true,
+        type: String,
+        default: "student",
+        validate: {
+            validator: role => roles.includes(role),
+            message: "Invalid Role"
+        }
+    }
+});
+
+const User = mongoose.model("User",UserSchema);
+module.exports = User;
