d4927510d3a4d3b04840ea4df3c4819a7429cf5a
luqmanMohammed <mohammed.luqman@pearson.com>
2019-11-01 01:47:04 +0530

    Add revoke token function

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -1,29 +1,74 @@
 const jwtHelper = require("../utils/JWTHelper");
 const { JWT_SECRET } = process.env;
+const RedisHelper = require("../utils/RedisHelper");
 class TokenController {
   async introspectToken(req, res, next) {
     const { token } = req.body;
-    let splitToken = null
+    let splitToken = null;
     try {
-        splitToken = jwtHelper.splitToken(token);
-    }catch (e) {
-        console.log("token =>" + splitToken);
-        return res.status(400).send("Invalid Token. Make Sure its a Bearer Token");
+      splitToken = jwtHelper.splitToken(token);
+    } catch (e) {
+      console.log("token =>" + splitToken);
+      return res
+        .status(400)
+        .send("Invalid Token. Make Sure its a Bearer Token");
     }
-    
-    console.log(splitToken)
+
+    console.log(splitToken);
     try {
-      const { role, email } = await jwtHelper.verifyToken(splitToken, JWT_SECRET);
+      const { role, email, jti } = await jwtHelper.verifyToken(
+        splitToken,
+        JWT_SECRET
+      );
+      try {
+        if (await RedisHelper.isRevoked(email, jti))
+          return res.status(200).json({
+              active: false
+          })
+      } catch (redisError) {
+        console.error(redisError);
+        return res.status(500).send("System Error, Contact Support");
+      }
       return res.status(200).json({
         role,
         active: true
       });
     } catch (e) {
-        console.log(e)
+      console.log(e);
       return res.status(200).json({
         active: false
       });
     }
+  }
+  async revokeToken(req,res,next) {
+    const { token } = req.body;
+    let splitToken = null;
+    try {
+      splitToken = jwtHelper.splitToken(token);
+    } catch (e) {
+      console.log("token =>" + splitToken);
+      return res
+        .status(400)
+        .send("Invalid Token. Make Sure its a Bearer Token");
+    }
+
+    console.log(splitToken);
+    try {
+      const { role, email, jti } = await jwtHelper.verifyToken(
+        splitToken,
+        JWT_SECRET
+      );
+      try {
+        await RedisHelper.addRevokedTokenJTI(email,jti);
+      } catch (redisError) {
+        console.error(redisError);
+        return res.status(500).send("System Error, Contact Support");
+      }
+      return res.status(200).send("Revoked");
+    } catch (e) {
+      return res.status(200).send("Revoked");
+    }
+      
   }
 }
 module.exports = new TokenController();
