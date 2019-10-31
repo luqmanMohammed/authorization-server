644df690b4aa8a0b91eae43b93fd985f2d083a63
luqmanMohammed <mohammed.luqman@pearson.com>
2019-10-31 12:04:14 +0530

    Add MongoDB Connection

--- server.js
+++ server.js
@@ -5,29 +5,36 @@ const mongoose = require("mongoose");
 
 const result = dotenv.config();
 if (result.error) {
-    console.error(result.error);
-    process.exit(1);
+  console.error(result.error);
+  process.exit(1);
 }
-const { PORT,DB_URI } = process.env
+const { PORT, DB_URI } = process.env;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT, err => {
-    if(err) {
-        console.error(err);
-        process.exit(2);
-    }
-    console.log(`Authorization Server Started in PORT ${PORT}`);
-    mongoose.connect(DB_URI, {
-        useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false 
+app.listen(PORT, async err => {
+  if (err) {
+    console.error(err);
+    process.exit(2);
+  }
+  console.log(`Authorization Server Started in PORT ${PORT}`);
+  try {
+    await mongoose.connect(DB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
     });
-})
+  } catch (e) {
+    console.error(e)
+    process.exit(3)
+  }
+});
 
 const db = mongoose.connection;
 db.once("open", () => {
-    console.log(`MongoDB Connection Established`);
-    // DO INDEX ROUTING
-})
-db.once("error", err => console.error(err));
+  console.log(`MongoDB Connection Established`);
+  // DO INDEX ROUTING
+});
