ace166160c95930ba141a7fdc19f0bf705bea478
luqmanMohammed <mohammed.luqman@pearson.com>
2019-10-31 20:43:24 +0530

    Add token logging

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -3,7 +3,14 @@ const { JWT_SECRET } = process.env;
 class TokenController {
   async introspectToken(req, res, next) {
     const { token } = req.body;
-    const splitToken = jwtHelper.splitToken(token);
+    let splitToken = null
+    try {
+        splitToken = jwtHelper.splitToken(token);
+    }catch (e) {
+        console.log("token =>" + splitToken);
+        return res.status(400).send("Invalid Token. Make Sure its a Bearer Token");
+    }
+    
     console.log(splitToken)
     try {
       const { role, email } = await jwtHelper.verifyToken(splitToken, JWT_SECRET);
